6fb86b256a28b2515c82096fac8bd605f66fe58d
olegstan <olegstan@inbox.ru>
2023-10-30 11:31:42 +0300

    default export

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Api from "src/Api";
+import ApiProxy from "src/ApiProxy";
+import ApiRequest from "src/ApiRequest";
+import Binding from "src/Binding";
+import Builder from "src/Builder";
+import NewApi from "src/NewApi";
+import NewApiRequest from "src/NewApiRequest";
+import UrlBind from "src/UrlBind";
+
+
+
+export {
+  Api,
+  ApiProxy,
+  ApiRequest,
+  Binding,
+  Builder,
+  NewApi,
+  NewApiRequest,
+  UrlBind,
+}
