522ee1a7c2aa15caa9c5b12480a891ff77083303
olegstan <olegstan@inbox.ru>
2023-10-30 11:47:38 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import Api from "src/Api";
-import ApiProxy from "src/ApiProxy";
-import ApiRequest from "src/ApiRequest";
-import Binding from "src/Binding";
-import Builder from "src/Builder";
-import NewApi from "src/NewApi";
-import NewApiRequest from "src/NewApiRequest";
-import UrlBind from "src/UrlBind";
+import Api from "./Api";
+import ApiProxy from "./ApiProxy";
+import ApiRequest from "./ApiRequest";
+import Binding from "./Binding";
+import Builder from "./Builder";
+import NewApi from "./NewApi";
+import NewApiRequest from "./NewApiRequest";
+import UrlBind from "./UrlBind";
 
 
 
